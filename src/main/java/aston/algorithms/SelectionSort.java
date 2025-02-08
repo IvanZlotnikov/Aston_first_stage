75a291e0055ca7f866bb43ccacc27bd83cbf3e1d
IvZlo <learn.java4@mail.ru>
2025-02-08 05:09:07 +0300

    add impl interface Sortable

--- src/main/java/aston/algorithms/SelectionSort.java
+++ src/main/java/aston/algorithms/SelectionSort.java
@@ -1,4 +1,12 @@
 package aston.algorithms;
 
-public class SelectionSort {
+import aston.core.Sortable;
+
+//для сортировки выбором
+public class SelectionSort<T> implements Sortable<T> {
+
+    @Override
+    public void sort(T[] array) {
+
+    }
 }

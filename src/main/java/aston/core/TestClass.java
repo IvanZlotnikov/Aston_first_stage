00618e19b5a9a3509159bf2b5302a82cca0647cd
konstantinmakarenko <konstantinmakarenko6064@gmail.com>
2025-02-08 11:05:56 +0600

    Тестовый commit. Попытка 2

--- src/main/java/aston/core/TestClass.java
+++ src/main/java/aston/core/TestClass.java
@@ -2,6 +2,6 @@ package aston.core;
 
 public class TestClass {
     public static void main(String[] args) {
-        System.out.println("This is a test class");
+        System.out.println("This is a test class!");
     }
 }

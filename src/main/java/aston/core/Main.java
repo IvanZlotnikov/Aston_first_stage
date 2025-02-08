5a2695217e1071fe3d78e6ff0bbccf7f3987441e
konstantinmakarenko <konstantinmakarenko6064@gmail.com>
2025-02-08 11:40:28 +0600

    Revert "Тестовый commit. Попытка 2"
    This reverts commit 00618e19b5a9a3509159bf2b5302a82cca0647cd.

--- src/main/java/aston/core/Main.java
+++ src/main/java/aston/core/Main.java
@@ -3,6 +3,6 @@ package aston.core;
 public class Main {
     // Тестовый комментарий
     public static void main(String[] args) {
-        System.out.println("Loren ipsum dolor");
+        System.out.println("Loren ipsum dolor!");
     }
 }

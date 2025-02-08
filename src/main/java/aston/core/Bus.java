4cb80fa0f064bf61a61885eddb5f217a37d407cd
konstantinmakarenko <konstantinmakarenko6064@gmail.com>
2025-02-08 12:14:42 +0600

    Разработан класс Bus с паттерном Builder

--- src/main/java/aston/core/Bus.java
+++ src/main/java/aston/core/Bus.java
@@ -1,4 +1,61 @@
 package aston.core;
 
-public class Bus {
+class Bus {
+    private final short number;
+    private final String model;
+    private final int mileage;
+
+    private Bus(short number, String model, int mileage) {
+        this.number = number;
+        this.model = model;
+        this.mileage = mileage;
+    }
+
+    public short getNumber() {
+        return number;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    // Builder класс
+    public static class BusBuilder {
+        private short number;
+        private String model;
+        private int mileage;
+
+        public BusBuilder number(short number) {
+            this.number = number;
+            return this;
+        }
+
+        public BusBuilder model(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public BusBuilder mileage(int mileage) {
+            this.mileage = mileage;
+            return this;
+        }
+
+        Bus build() {
+            if (number <= 0) {
+                throw new IllegalArgumentException("Номер автобуса не может быть меньше нуля или равен нулю");
+            }
+
+            if (model == null || model.isEmpty()) {
+                throw new IllegalArgumentException("Модель автобуса не может быть пустой");
+            }
+            if (mileage < 0) {
+                throw new IllegalArgumentException("Пробег не может быть отрицательным");
+            }
+            return new Bus(number, model, mileage);
+        }
+    }
 }

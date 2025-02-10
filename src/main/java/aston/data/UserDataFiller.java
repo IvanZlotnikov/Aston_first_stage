aa145ecc85f115d95aa658c88ed1a33a93ef8c7a
IvZlo <learn.java4@mail.ru>
2025-02-10 15:08:59 +0300

    добавил регулярныые выражения с помощью Кирила и Pattern.compile с помощью Matcher ищем совпадения в строке

--- src/main/java/aston/data/UserDataFiller.java
+++ src/main/java/aston/data/UserDataFiller.java
@@ -4,8 +4,11 @@ import aston.core.DataFiller;
 import aston.model.User;
 import aston.utils.FileReaderUtill;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserDataFiller implements DataFiller<User> {
-    private String filePath;
+    private final String filePath;
 
     public UserDataFiller(String filePath) {
         this.filePath = filePath;
@@ -15,13 +18,16 @@ public class UserDataFiller implements DataFiller<User> {
     public User[] fillData() {
         String[] lines = FileReaderUtill.readFile(filePath);
         User[] users = new User[lines.length];
+        Pattern pattern = Pattern.compile("(\\d+) (\\w+) (\\d+)");
         for (int i = 0; i < lines.length; i++) {
-            String[] splits = lines[i].split(",");
-            users[i] = new User.Builder()
-                    .setName(splits[0])
-                    .setPassword(splits[1])
-                    .setEmail(splits[2])
-                    .build();
+            Matcher matcher = pattern.matcher(lines[i]);
+            if (matcher.find()) {
+                users[i] = new User.Builder()
+                        .setName(matcher.group(0))
+                        .setPassword(matcher.group(1))
+                        .setEmail(matcher.group(2))
+                        .build();
+            }
         }
         return users;
     }

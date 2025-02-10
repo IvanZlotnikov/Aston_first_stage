67b9f7e12c755ed718277dca638ea7ee557d5cec
IvZlo <learn.java4@mail.ru>
2025-02-10 03:26:46 +0300

    добавил чистку файла

--- src/main/java/aston/utils/FileWriterUtill.java
+++ src/main/java/aston/utils/FileWriterUtill.java
@@ -6,13 +6,20 @@ import java.io.IOException;
 
 public class FileWriterUtill {
 
-    public void writeToFile(String data, String filePath) {
+    public void writeToFile(String data, String filePath, boolean append) {
         if (data == null || filePath == null || data.isEmpty() || filePath.isEmpty())
             throw new IllegalArgumentException("data or path is null or empty");
 
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filePath, true))) {
             bufferedWriter.write(data);
             bufferedWriter.newLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void clearFile(String filePath) {
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(filePath,false))){
+            bw.write("");
         } catch (IOException e) {
             e.printStackTrace();
         }

19fc0fb1211d2f828405ebf87addb6e78b554ef5
IvZlo <learn.java4@mail.ru>
2025-02-10 03:23:56 +0300

    читаем с файла и храним в массиве

--- src/main/java/aston/data/StudentDataFiller.java
+++ src/main/java/aston/data/StudentDataFiller.java
@@ -2,14 +2,27 @@ package aston.data;
 
 import aston.core.DataFiller;
 import aston.model.Student;
+import aston.utils.FileReaderUtill;
 
 public class StudentDataFiller implements DataFiller<Student> {
+    private String filePath;
+
+    public StudentDataFiller(String filePath) {
+        this.filePath = filePath;
+    }
+
     @Override
     public Student[] fillData() {
-        return new Student[]{
-                new Student.Builder().setGroupNumber(11).setAverageGrade(5.5).setRecordBookNumber(2).build(),
-                new Student.Builder().setGroupNumber(12).setAverageGrade(4.5).setRecordBookNumber(1).build(),
-                new Student.Builder().setGroupNumber(13).setAverageGrade(6.5).setRecordBookNumber(3).build(),
-        };
+        String[] lines = FileReaderUtill.readFile(filePath);
+        Student[] students = new Student[lines.length];
+        for (int i = 0; i < lines.length; i++) {
+            String[] splits = lines[i].split(",");
+            students[i] = new Student.Builder()
+                    .setGroupNumber(Integer.parseInt(splits[0]))
+                    .setAverageGrade(Double.parseDouble(splits[1]))
+                    .setRecordBookNumber(Integer.parseInt(splits[2]))
+                    .build();
+        }
+        return students;
     }
 }

398699700f14074b8ca7e0f477a1b8876fcf44d4
IvZlo <learn.java4@mail.ru>
2025-02-10 14:59:47 +0300

    добавил регулярныые выражения с помощью Кирила и Pattern.compile с помощью Matcher ищем совпадения в строке

--- src/main/java/aston/data/BusDataFiller.java
+++ src/main/java/aston/data/BusDataFiller.java
@@ -4,6 +4,9 @@ import aston.core.DataFiller;
 import aston.model.Bus;
 import aston.utils.FileReaderUtill;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class BusDataFiller implements DataFiller<Bus> {
     private String filePath;
@@ -16,13 +19,16 @@ public class BusDataFiller implements DataFiller<Bus> {
     public Bus[] fillData() {
         String[] lines = FileReaderUtill.readFile(filePath);
         Bus[] buses = new Bus[lines.length];
+        Pattern pattern = Pattern.compile("(\\d+) (\\w+) (\\d+)");
         for (int i = 0; i < lines.length; i++) {
-            String[] splits = lines[i].split(",");
-            buses[i] = new Bus.Builder()
-                    .setNumber(Integer.parseInt(splits[0]))
-                    .setModel(splits[1])
-                    .setMileage(Integer.parseInt(splits[2]))
-                    .build();
+            Matcher matcher = pattern.matcher(lines[i]);
+            if (matcher.find()) {
+                buses[i] = new Bus.Builder()
+                        .setNumber(Integer.parseInt(matcher.group(0)))
+                        .setModel(matcher.group(1))
+                        .setMileage(Integer.parseInt(matcher.group(2)))
+                        .build();
+            }
         }
         return buses;
     }

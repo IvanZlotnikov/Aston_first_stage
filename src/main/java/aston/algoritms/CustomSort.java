8fd797a090c37573696f98d9b9047c69f4988945
IvZlo <learn.java4@mail.ru>
2025-02-14 10:31:29 +0300

    подсмотрел как сделать сортировку по любому типу поля

--- src/main/java/aston/algoritms/CustomSort.java
+++ src/main/java/aston/algoritms/CustomSort.java
@@ -1,13 +1,20 @@
 package aston.algoritms;
 
-import java.util.function.ToIntFunction;
+import java.util.Comparator;
+import java.util.function.Function;
 
+//Класс для выполнения пользовательской сортировки.
 public class CustomSort {
-    public static <T extends Comparable<T>> void sort(T[] array, ToIntFunction<T> keyExtractor) {
+    public static <T, U extends Comparable<? super U>> void sort(T[] array, Function<? super T, ? extends U> keyExtractor) {
+        Comparator<T> comparator = Comparator.comparing(keyExtractor);
+        sort(array, comparator);
+    }
+
+    public static <T> void sort(T[] array, Comparator<? super T> comparator) {
         for (int i = 0; i < array.length - 1; i += 2) { // Идем по четным индексам массива
             int minIndex = i;
             for (int j = i + 2; j < array.length; j += 2) { // Идем по четным индексам массива
-                if (keyExtractor.applyAsInt(array[j]) < keyExtractor.applyAsInt(array[minIndex])) {
+                if (comparator.compare(array[j], array[minIndex]) < 0) {
                     minIndex = j;
                 }
             }

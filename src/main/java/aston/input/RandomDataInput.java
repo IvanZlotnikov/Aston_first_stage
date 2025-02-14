f3b5d5e327fef5da8c9c70fa62ef30e1d6d0517c
IvZlo <learn.java4@mail.ru>
2025-02-14 10:57:24 +0300

    обработка случайного ввода любого типа

---
+++ src/main/java/aston/input/RandomDataInput.java
@@ -0,0 +1,27 @@
+package aston.input;
+
+import aston.entity.Bus;
+import aston.entity.Student;
+import aston.entity.User;
+import aston.utill.RandomDataGenerator;
+
+public class RandomDataInput<T> implements DataInput<T> {
+    private final Class<T> type;
+
+    public RandomDataInput(Class<T> type) {
+        this.type = type;
+    }
+
+    @Override
+    public T[] input(int size) {
+        if (Bus.class.isAssignableFrom(type)) {
+            return (T[]) RandomDataGenerator.generateRandomBuses(size);
+        } else if (Student.class.isAssignableFrom(type)) {
+            return (T[]) RandomDataGenerator.generateRandomStudents(size);
+        } else if (User.class.isAssignableFrom(type)) {
+            return (T[]) RandomDataGenerator.generateRandomUsers(size);
+        }
+        throw new IllegalArgumentException("Unknown data type");
+    }
+}
+
